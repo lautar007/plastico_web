7687855f2b6f45e75b735b271becc5848ba9b598
lautaro <kautarol@gmail.com>
2022-04-25 17:57:15 -0300

    desarrollando galerías

--- client/plastico-web/src/components/Galeria.js
+++ client/plastico-web/src/components/Galeria.js
@@ -22,6 +22,24 @@ export default function Galeria () {
     return(
         <div>
             <h1>Galería de imágenes</h1>
+            <div className="cont-gal">
+
+                <ul className="slider">
+                  <li id = 'slider1'>
+                      <img src={gal[0]}/>
+                  </li>
+                  <li id = 'slider2'>
+                      <img src={gal[1]}/>
+                  </li>
+                  <li id = 'slider3'>
+                      <img src={gal[2]}/>  
+                  </li>
+                  <li id = 'slider4'>
+                      <img src={gal[3]}/>
+                  </li>
+                </ul>
+
+            </div>
         </div>
     )
 }
