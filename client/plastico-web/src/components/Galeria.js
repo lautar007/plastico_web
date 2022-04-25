551e04bbd96a407ff02342a74e897ac496885b9c
lautaro <kautarol@gmail.com>
2022-04-25 17:49:24 -0300

    desarrollando galerias

--- client/plastico-web/src/components/Galeria.js
+++ client/plastico-web/src/components/Galeria.js
@@ -1,13 +1,27 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { getPublicacion } from "../actions/actions";
+import { useEffect } from "react";
 
 export default function Galeria () {
     
     let {id} = useParams(); 
 
+    const dispatch = useDispatch();
+    const publi = useSelector((state)=>state.Publicacion);
+    console.log(publi);
+ 
+    const gal = publi.galeria
+    console.log(gal)
+
+    useEffect(()=>{
+        dispatch(getPublicacion(id)); 
+    }, [dispatch]);
+
     return(
         <div>
-            <h1>Esta es la galería de la publicación: {id} </h1>
+            <h1>Galería de imágenes</h1>
         </div>
     )
 }
