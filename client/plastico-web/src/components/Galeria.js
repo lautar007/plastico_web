43268593eb672d14aa452f82b981b31007181817
lautaro <kautarol@gmail.com>
2022-05-28 18:53:46 -0300

    galeria li

--- client/plastico-web/src/components/Galeria.js
+++ client/plastico-web/src/components/Galeria.js
@@ -40,19 +40,19 @@ export default function Galeria () {
 
                 <ul className="slider">
                   <li id = 'slider1'>
-                      <img src={gal? gal[0]: logo}/>
+                      <img className="gal-imagen" src={gal? gal[0]: logo}/>
                   </li>
                   <li id = 'slider2'>
-                      <img src={gal? gal[1]: logo}/>
+                      <img className="gal-imagen" src={gal? gal[1]: logo}/>
                   </li>
                   <li id = 'slider3'>
-                      <img src={gal? gal[2]: logo}/>  
+                      <img className="gal-imagen" src={gal? gal[2]: logo}/>  
                   </li>
                   <li id = 'slider4'>
-                      <img src={gal? gal[3]: logo}/>
+                      <img className="gal-imagen" src={gal? gal[3]: logo}/>
                   </li>
                   <li id = 'slider5'>
-                      <img src={gal? gal[4]: logo}/>
+                      <img className="gal-imagen" src={gal? gal[4]: logo}/>
                   </li>
                 </ul>
 
