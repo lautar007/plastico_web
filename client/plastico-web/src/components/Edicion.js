fa2ddac087eba6d3330b4ba84c24e66818cee887
Lautaro <kautarol@gmail.com>
2022-06-04 20:27:29 -0300

    desarrollando put

--- client/plastico-web/src/components/Edicion.js
+++ client/plastico-web/src/components/Edicion.js
@@ -19,98 +19,9 @@ export default function PostDetail(){
         dispatch(getPublicacion(id)); 
     }, [dispatch]);
 
-
-    function handleLink (){
-      if(publi.categoria === 'artistico'){
-          return '/artistic'
-      }
-      if(publi.categoria === 'comercial'){
-          return '/comercial'
-      }
-      else{
-          return '/home'
-      }
-    }
-
-    <iframe width="1350" height="480" src="https://www.youtube.com/embed/6r2aucghTUY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-
-    function video(){
-        console.log(publi.galeria[publi.galeria.length -1])
-        if(publi.galeria[publi.galeria.length -1].includes('www.youtube.com')){
-            let videito = publi.galeria[publi.galeria.length -1].slice(32,43);
-            return (
-                <div>
-                     <iframe width="484" height="480" src={`https://www.youtube.com/embed/${videito}`} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                </div>
-            )
-        }
-        else return (
-            <div>
-                <p>Esta publicación no contiene ningún video</p>
-            </div>
-        )
-    }
-
-
     return(
         <div>
-            <div className="contPost">
-                <img id="img-post" src={publi.imagen}/>
-                <div id="contPostInfo">
-                    <h1>{publi.titulo}</h1>
-                    <hr/>
-                    <h2>{publi.subtitulo}</h2>
-                    <p>{publi.contenido}</p>
-                    <hr/>
-                    <p1>Fecha de publicación: {publi.fecha}</p1>
-                    <p1>Galería de fotos, abajo ↓</p1>
-                    <Link to={handleLink()}>
-                        <button className="B-volver">Volver</button>
-                    </Link>
-                </div>
-            </div>
-            <div>
-                <h1>Galería</h1>
-                <div className="cont-galeria-post">
-                {
-                    publi.galeria && publi.galeria.length != 0 ?
-                    <div>
-                    {
-                publi.galeria.map(img =>{
-                    if(img.includes('jpg') || img.includes('png')){
-                    return(
-                            <Link to={ '/galeria/' + id + '#slider1'}>
-                                <img id="img-blog" key={Math.random()} src={img}></img>
-                            </Link>
-                    )
-                    }
-                }) 
-                    }
-                <img src={LOGO} id='img-blog-logo'/>
-                </div>
-                    :
-                    <div>
-                    <h2>Esta publicación no contiene una galería de imágenes</h2>
-                    <hr/>
-                    </div>
-                }
-                </div>
-                
-                <div>
-                <h1>Video</h1>
-                <div className="cont-galeria-post">
-                {
-                   publi.galeria && publi.galeria.length != 0 ?
-                   video()
-                   :
-                   <div>
-                   <h2>Esta publicación no contiene ningún video</h2>
-                   <hr/>
-                   </div>
-                }
-                </div>
-            </div>
-            </div>
+            <h1>Edición de post: {publi.titulo}</h1>
         </div>
     )
 }
