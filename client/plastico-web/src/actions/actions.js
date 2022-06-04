9fd258e5d29d28567f117d7e12661289ae82dc43
Lautaro <kautarol@gmail.com>
2022-06-04 20:06:53 -0300

    deploy put

--- client/plastico-web/src/actions/actions.js
+++ client/plastico-web/src/actions/actions.js
@@ -98,7 +98,7 @@ export function putPublicacion(id, payload){
         })
         return dispatch({
             type: 'PUT_PUBLICACION',
-            payload: json.data
+            payload: data
         })
     }
 }
