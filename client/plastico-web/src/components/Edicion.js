8b23d18b12f445198a5e1af1b6446b95f18e7455
Lautaro <kautarol@gmail.com>
2022-06-05 22:01:00 -0300

    desarrollando edicion

--- client/plastico-web/src/components/Edicion.js
+++ client/plastico-web/src/components/Edicion.js
@@ -20,7 +20,7 @@ export default function PostDetail(){
 
     const [input, setInput] = useState({
         titulo: '',
-        fecha: fecha,
+        fecha: '',
         contenido: '',
         imagen:'',
         categoria:'',
