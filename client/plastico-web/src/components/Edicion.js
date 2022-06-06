a90cdedfc149860f820e8df8eb5423244266bc3d
Lautaro <kautarol@gmail.com>
2022-06-05 21:59:09 -0300

    desarrollando edición

--- client/plastico-web/src/components/Edicion.js
+++ client/plastico-web/src/components/Edicion.js
@@ -8,18 +8,14 @@ import './Edicion.css'
 export default function PostDetail(){
 
     let {id} = useParams();
-    console.log(id)
-
-    const hoy = new Date()
-    const fecha = hoy.getDate() + '-' + ( hoy.getMonth() + 1 ) + '-' + hoy.getFullYear();
     
     const dispatch = useDispatch();
-    useEffect(()=>{
-        dispatch(getPublicacion(id)); 
-    }, [dispatch, id]);
-
     const publi = useSelector((state)=>state.Publicacion);
     console.log(publi);
+
+    useEffect(()=>{
+        dispatch(getPublicacion(id)); 
+    }, [dispatch]);
 
 
     const [input, setInput] = useState({
