63262b224ae891be22a7d6c82e83f58271375c07
Lautaro <kautarol@gmail.com>
2022-06-10 15:05:04 -0300

    mejorando muchísimo las galerias

--- client/plastico-web/src/components/Galeria.js
+++ client/plastico-web/src/components/Galeria.js
@@ -30,35 +30,89 @@ export default function Galeria () {
   
     return(
         <div>
-            <h1>Galería de imágenes</h1>
-            <Link to = {'/artistic/' + id} >
-            <button className="btn-contraseña">Volver</button>
-            </Link>
-            <div className="cont-gal">
+        <h1>Galería de imágenes</h1>
+        <Link to = {'/artistic/' + id} >
+        <button className="btn-contraseña">Volver</button>
+        </Link>
+        <div className="cont-gal">
 
-                <a className="flecha" href={cont === '1'? '#slider5' : '#slider' + (cont - 1)}>←</a>
+            <ul id = 'galeria-M'>
+              <li>
+                  <a href ='#0'><img id= 'img-blog' src={gal[0]? gal[0]: logo}/></a>
+              </li>
+              <li>
+                  <a href="#1"><img id= 'img-blog' src={gal[1]? gal[1]: logo}/></a>
+              </li>
+              <li>
+                  <a href="#2"><img id= 'img-blog' src={gal[2]? gal[2]: logo}/></a>  
+              </li>
+              <li>
+                  <a href="#3"><img id= 'img-blog' src={gal[3]? gal[3]: logo}/></a>
+              </li>
+              <li>
+                  <a href="#4"><img id= 'img-blog' src={gal[4]? gal[4]: logo}/></a>
+              </li>
+              <li>
+                  <a href="#5"><img id= 'img-blog' src={gal[5]? gal[5]: logo}/></a>
+              </li>
+            </ul>
 
-                <ul className="slider">
-                  <li id = 'slider1'>
-                      <img className="gal-imagen" src={gal? gal[0]: logo}/>
-                  </li>
-                  <li id = 'slider2'>
-                      <img className="gal-imagen" src={gal? gal[1]: logo}/>
-                  </li>
-                  <li id = 'slider3'>
-                      <img className="gal-imagen" src={gal? gal[2]: logo}/>  
-                  </li>
-                  <li id = 'slider4'>
-                      <img className="gal-imagen" src={gal? gal[3]: logo}/>
-                  </li>
-                  <li id = 'slider5'>
-                      <img className="gal-imagen" src={gal? gal[4]: logo}/>
-                  </li>
-                </ul>
-
-                <a className="flecha" href={cont === '5'? '#slider1' : '#slider' + contR}>→</a>
-
-            </div>
         </div>
+
+        <div className="modal" id="0">
+            <div className="imagen-modal">
+                <a className="arrow" href="#5"> ⇐</a>
+                <img src={gal[0]? gal[0]: logo}/>
+                <a className="arrow" href="#1"> ⇒</a>
+            </div>
+            <a className="cerrar-modal" href="">X</a>
+        </div>
+
+        <div className="modal" id="1">
+            <div className="imagen-modal">
+                <a className="arrow" href="#0"> ⇐</a>
+                <img src={gal[1]? gal[1]: logo}/>
+                <a className="arrow" href="#2"> ⇒</a>
+            </div>
+            <a className="cerrar-modal" href="">X</a>
+        </div>
+
+        <div className="modal" id="2">
+            <div className="imagen-modal">
+                <a className="arrow" href="#1"> ⇐</a>
+                <img src={gal[2]? gal[2]: logo}/>
+                <a className="arrow" href="#3"> ⇒</a>
+            </div>
+            <a className="cerrar-modal" href="">X</a>
+        </div>
+
+        <div className="modal" id="3">
+            <div className="imagen-modal">
+                <a className="arrow" href="#2"> ⇐</a>
+                <img src={gal[3]? gal[3]: logo}/>
+                <a className="arrow" href="#4"> ⇒</a>
+            </div>
+            <a className="cerrar-modal" href="">X</a>
+        </div>
+
+        <div className="modal" id="4">
+            <div className="imagen-modal">
+                <a className="arrow" href="#3"> ⇐</a>
+                <img src={gal[4]? gal[4]: logo}/>
+                <a className="arrow" href="#5"> ⇒</a>
+            </div>
+            <a className="cerrar-modal" href="">X</a>
+        </div>
+
+        <div className="modal" id="5">
+            <div className="imagen-modal">
+                <a className="arrow" href="#4"> ⇐</a>
+                <img src={gal[5]? gal[5]: logo}/>
+                <a className="arrow" href="#0"> ⇒</a>
+            </div>
+            <a className="cerrar-modal" href="">X</a>
+        </div>
+
+    </div>
     )
 }
