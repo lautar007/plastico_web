a68add4762e723c0bf3702f71fccf489d386b2f1
Lautaro <kautarol@gmail.com>
2022-09-15 19:51:21 +0400

    detalles de categorías

--- client/plastico-web/src/components/Card.js
+++ client/plastico-web/src/components/Card.js
@@ -4,13 +4,19 @@ import './Card.css';
 
 
 export default function Card({titulo, imagen, id, subtitulo}){
+    
+    let tituloResumido = titulo;
+
+    if(titulo.length > 20){
+        tituloResumido = titulo.slice(0, 20) + '...'
+    }
 
     return(
         <div className="img-container">
             <Link to= {'/artistic/' + id}>
                 <img src={imagen} alt="img not found"></img>
                 <h3>{subtitulo.slice(0, 40) + '...'}</h3>
-                <h2 className="card-titulo">{titulo}</h2>
+                <h2 className="card-titulo">{tituloResumido}</h2>
             </Link>
         </div>
     )
